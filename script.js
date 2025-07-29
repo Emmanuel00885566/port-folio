330c461b4deb8e663a15eb33bde48292331f4199
Emmanuel0085566-github <emmanuelniyi1997@gmail.com>
2025-07-30 00:31:14 +0100

    Add the changes

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function toggleTheme() {
   document.body.classList.toggle('light-mode');
 }
+function toggleMenu() {
+  const nav = document.querySelector('nav');
+  nav.classList.toggle('open');
+}
